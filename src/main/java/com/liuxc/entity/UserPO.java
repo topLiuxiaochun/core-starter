9eb83d0e1f8a77f0354dfe0e3d0bbacb6a362f84
topLiuxiaochun <thinkpad>
2017-10-16 22:55:37 +0800

    提交基本的代码

---
+++ src/main/java/com/liuxc/entity/UserPO.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.liuxc.entity;
+
+/**
+ * 
+ * @since:2017年10月16日
+ * @author:liuxc
+ */
+public class UserPO {
+
+	private String userId;
+	
+	private String userName;
+	
+	private String nickName;
+	
+	private String sex;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
